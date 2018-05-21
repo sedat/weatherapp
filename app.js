9a05fddf9ff5213bef2ebb5e78eb004633f3c616
Sedat Can Yalçın <sedatcyalcin@gmail.com>
2018-05-21 17:15:17 +0300

    Chart feature, more weather information including forecasts added

--- app.js
+++ app.js
@@ -11,13 +11,20 @@ app.get("/", (req, res) => {
     res.render("search");
 })
 
+
 app.get("/weather", (req,res) => {
-    
+    var forecast;
     let city = req.query.city;
-    request("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + mykey + "&units=metric", function(error, response, body) {
+    request("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+ myID + "&units=metric", function(error, response, body) {
         if(!error && response.statusCode == 200){
         let data = JSON.parse(body);
-        res.render("index", {data: data})
+        request("http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+ myID + "&units=metric", function(error, response, body) {
+            if(!error && response.statusCode == 200){
+            forecast = JSON.parse(body);
+            res.render("index", {data: data, forecast: forecast})
+            } 
+        })
+        
         } 
     })
     
